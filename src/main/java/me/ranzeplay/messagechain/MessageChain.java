5f0886093af65e475c49ed706a21a38f30ad5226
Jeb Feng <ranzeplay@outlook.com>
2023-10-08 23:54:39 +0800

    Initial commit, add template files

---
+++ src/main/java/me/ranzeplay/messagechain/MessageChain.java
@@ -0,0 +1,10 @@
+package me.ranzeplay.messagechain;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MessageChain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/ranzeplay/messagechain/client/MessageChainClient.java
@@ -0,0 +1,10 @@
+package me.ranzeplay.messagechain.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MessageChainClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
