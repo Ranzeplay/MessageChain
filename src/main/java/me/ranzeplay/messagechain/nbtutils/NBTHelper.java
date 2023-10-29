5f6e40a2b16cfe8e38f204daf06794ef4371bd33
Jeb Feng <ranzeplay@outlook.com>
2023-10-29 16:04:42 +0800

    Implement NBT deserializer

--- src/main/java/me/ranzeplay/messagechain/nbtutils/NBTHelper.java
+++ src/main/java/me/ranzeplay/messagechain/nbtutils/NBTHelper.java
@@ -4,8 +4,9 @@ import lombok.SneakyThrows;
 import net.minecraft.nbt.*;
 
 import java.lang.reflect.Field;
-import java.util.AbstractList;
-import java.util.Objects;
+import java.lang.reflect.ParameterizedType;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class NBTHelper {
     @SneakyThrows
@@ -32,8 +33,7 @@ public class NBTHelper {
                 field.setAccessible(true);
                 if (field.isAnnotationPresent(NBTSerializableEntry.class)) {
                     var element = serializeField(source, field);
-                    var entryAnnotation = field.getAnnotation(NBTSerializableEntry.class);
-                    result.put(Objects.equals(entryAnnotation.key(), "") ? field.getName() : entryAnnotation.key(), element);
+                    result.put(getNbtFieldName(field), element);
                 }
             }
 
@@ -62,23 +62,108 @@ public class NBTHelper {
             return NbtFloat.of((float) source);
         } else if (typeClass.isAssignableFrom(Double.class)) {
             return NbtDouble.of((double) source);
-        } else if (typeClass.isAssignableFrom(Integer[].class)) {
+        } else if (typeClass.isAssignableFrom(int[].class)) {
             return new NbtIntArray((int[]) source);
-        } else if (typeClass.isAssignableFrom(Byte[].class)) {
+        } else if (typeClass.isAssignableFrom(byte[].class)) {
             return new NbtByteArray((byte[]) source);
-        } else if (typeClass.isAssignableFrom(Long[].class)) {
+        } else if (typeClass.isAssignableFrom(long[].class)) {
             return new NbtLongArray((long[]) source);
         } else if (typeClass.isAssignableFrom(String.class)) {
             return NbtString.of(String.valueOf(source));
         } else if (typeClass.getSuperclass() == AbstractList.class) {
             AbstractList list = (AbstractList) source;
-            var obj = new NbtList();
-            for (var item : list) {
-                obj.add(serializeObject(item));
+            var obj = new NbtCompound();
+            // Put content type to the first
+            if(!list.isEmpty()) {
+                obj.putString("type", list.get(0).getClass().getTypeName());
+            } else {
+                obj.putString("type", "?");
             }
+            var nbtList = new NbtList();
+            for (var item : list) {
+                nbtList.add(serializeObject(item));
+            }
+            obj.put("items", nbtList);
             return obj;
         } else {
             return null;
         }
+    }
+
+    @SneakyThrows
+    public static <T> T deserialize(NbtCompound nbt, Class<T> targetType) {
+        return deserializeClass(nbt, targetType);
+    }
+
+    private static Object deserializeObject(NbtElement element, Class<?> targetType) {
+        var result = deserializeBasicType(element, targetType);
+        if (result == null) return deserializeClass((NbtCompound) element, targetType);
+        else return result;
+    }
+
+    @SneakyThrows
+    private static Object deserializeBasicType(NbtElement element, Class<?> targetType) {
+        if (targetType.isAssignableFrom(int.class)) {
+            return ((NbtInt) element).intValue();
+        } else if (targetType.isAssignableFrom(short.class)) {
+            return ((NbtShort) element).shortValue();
+        } else if (targetType.isAssignableFrom(long.class)) {
+            return ((NbtLong) element).longValue();
+        } else if (targetType.isAssignableFrom(float.class)) {
+            return ((NbtFloat) element).floatValue();
+        } else if (targetType.isAssignableFrom(double.class)) {
+            return ((NbtDouble) element).doubleValue();
+        } else if (targetType.isAssignableFrom(int[].class)) {
+            return ((NbtIntArray) element).getIntArray();
+        } else if (targetType.isAssignableFrom(byte[].class)) {
+            return ((NbtByteArray) element).getByteArray();
+        } else if (targetType.isAssignableFrom(long[].class)) {
+            return ((NbtLongArray) element).getLongArray();
+        } else if (targetType.isAssignableFrom(String.class)) {
+            return element.asString();
+        } else if (targetType.getSuperclass() == AbstractList.class) {
+            var comp = (NbtCompound) element;
+            var typeName = comp.getString("type");
+            var constructor = targetType.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            var result = (List) constructor.newInstance();
+
+            if(!typeName.equalsIgnoreCase("?")) {
+                var list = (NbtList) comp.get("items");
+                for (var item : list) {
+                    result.add(deserializeObject(item, Class.forName(typeName)));
+                }
+            }
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    @SneakyThrows
+    private static <T> T deserializeClass(NbtCompound nbt, Class<T> targetType) {
+        if (targetType.isAnnotationPresent(NBTSerializable.class)) {
+            var constructor = targetType.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            var result = constructor.newInstance();
+
+            var fields = Arrays.stream(targetType.getDeclaredFields())
+                    .filter(f -> f.isAnnotationPresent(NBTSerializableEntry.class))
+                    .collect(Collectors.toCollection(ArrayList::new));
+            for (var field : fields) {
+                var subElement = nbt.get(getNbtFieldName(field));
+                var clazz = field.getType();
+                field.set(result, deserializeObject(Objects.requireNonNull(subElement), clazz));
+            }
+
+            return result;
+        }
+        return null;
+    }
+
+    private static String getNbtFieldName(Field field) {
+        field.setAccessible(true);
+        var entryAnnotation = field.getAnnotation(NBTSerializableEntry.class);
+        return Objects.equals(entryAnnotation.key(), "") ? field.getName() : entryAnnotation.key();
     }
 }
--- src/test/java/me/ranzeplay/messagechain/NBTSerializationTest.java
+++ src/test/java/me/ranzeplay/messagechain/NBTSerializationTest.java
@@ -1,9 +1,11 @@
 package me.ranzeplay.messagechain;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import me.ranzeplay.messagechain.nbtutils.NBTHelper;
 import me.ranzeplay.messagechain.nbtutils.NBTSerializable;
 import me.ranzeplay.messagechain.nbtutils.NBTSerializableEntry;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -13,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class NBTSerializationTest {
     @AllArgsConstructor
     @NBTSerializable
+    @NoArgsConstructor
     static class MasterContent {
         @NBTSerializableEntry
         String a;
@@ -28,7 +31,9 @@ public class NBTSerializationTest {
 
     @AllArgsConstructor
     @NBTSerializable
+    @NoArgsConstructor
     static class SubContent {
+
         @NBTSerializableEntry
         int a;
         @NBTSerializableEntry
@@ -37,8 +42,10 @@ public class NBTSerializationTest {
         int[] arr;
     }
 
-    @Test
-    public void testSerialization() {
+    MasterContent content;
+
+    @BeforeEach
+    public void setup() {
         var data = new MasterContent("hello", 233, new ArrayList<>(), new ArrayList<>(), new SubContent(2, 3, new int[]{2, 3, 4}));
         data.stringArrayList.add("MessageChain");
         data.stringArrayList.add("hNation");
@@ -46,8 +53,20 @@ public class NBTSerializationTest {
         data.subContents.add(new SubContent(1, 4, new int[]{-2, -3, -4}));
         data.subContents.add(new SubContent(5, -2, new int[]{0, 0, 0}));
 
-        var result = NBTHelper.serialize(data);
+        content = data;
+    }
 
+    @Test
+    public void testSerialization() {
+        var result = NBTHelper.serialize(content);
         assertEquals(233, result.getLong("b"));
+    }
+
+    @Test
+    public void testDeserialization() {
+        var nbt = NBTHelper.serialize(content);
+        var result = NBTHelper.deserialize(nbt, MasterContent.class);
+
+        assertEquals(233, result.b);
     }
 }
