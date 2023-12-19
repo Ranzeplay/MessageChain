f7c564a763f208b7c316f379d080e2e064437450
Jeb Feng <69068007+Ranzeplay@users.noreply.github.com>
2023-12-19 23:56:47 +0800

    Update docusaurus.config.ts

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/icon.png',
 
   // Set the production url of your site here
-  url: 'https://msgc.ranzeplay.me',
+  url: 'https://msgc.ranzeplay.space',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
